433706112420cbd069dbfbb62be457b51fc89787
Constantine Tarasenkov <iamvfx@gmail.com>
2022-03-24 12:47:00 +0300

    Reverse facd43102bb8292c7d348c6e9aebc5ef6e6babd1

--- generate.sh
+++ generate.sh
@@ -1,1 +1,1 @@
-LD_PRELOAD=/opt/github/redgpu/framework/libxml2.so LD_LIBRARY_PATH=/opt/github/redgpu/framework ./generate > README.md
+LD_LIBRARY_PATH=/opt/github/redgpu/framework ./generate > README.md
